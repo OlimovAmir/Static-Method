226b6213e34c986dc1a37786d3555cd7dfb0d902
olimov Amir <olimov.amir@mail.ru>
2023-12-24 11:46:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+class MyClas
+{
+    public MyClas()
+    {
+        
+    }
+
+    public int MyProperty { get; set; }
+
+    private static int a;
+    private int b;
+
+    public static void Foo()
+    {
+        Console.WriteLine("Вызван метод Foo");
+    }
+
+    public void Bar()
+    {
+        Console.WriteLine("Вызван метод Bar");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClas myClas = new MyClas();
+
+        MyClas.Foo();
+
+        myClas.Bar();
+    }
+}
