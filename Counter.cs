ede00c84a483c706cc4dd745aebef1d9fd49d445
olimov Amir <olimov.amir@mail.ru>
2023-12-24 12:20:48 +0300

    changed

---
+++ Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Method
+{
+    internal class Counter
+    {
+        public Counter()
+        {
+            counter++;
+        }
+        private static int counter;
+
+		public static int CounterProperty
+		{
+			get { return counter; }
+			set { counter = value; }
+		}
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -7,14 +7,24 @@ class MyClas
         
     }
 
-    public int MyProperty { get; set; }
+    public static int MyProperty { get; set; }
 
     private static int a;
+
+    public static int A
+    {
+        get { return a; }
+        set { a = value; }
+    }
+
     private int b;
 
     public static void Foo()
     {
         Console.WriteLine("Вызван метод Foo");
+
+        //a = 11;
+        //Console.WriteLine(a);
     }
 
     public void Bar()
@@ -32,5 +42,16 @@ class Program
         MyClas.Foo();
 
         myClas.Bar();
+
+        MyClas.A = 25;
+
+        MyClas.MyProperty = 55;
+
+
+        Static_Method.Counter counter = new Static_Method.Counter();
+        Static_Method.Counter counter2= new Static_Method.Counter();
+        Static_Method.Counter counter3= new Static_Method.Counter();
+        Console.WriteLine(Static_Method.Counter.CounterProperty);
+        
     }
 }
